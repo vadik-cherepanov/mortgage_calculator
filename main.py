713c4462c11b70684b3cbb306cd86fa20ed4186b
Vadik <vadikschool248@gmail.com>
2022-02-06 10:57:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class MortgageCalculatorApp(MDApp):
+    def build(self):
+        return MDLabel(text='Hello, World!', halign='center')
+
+MortgageCalculatorApp().run()
